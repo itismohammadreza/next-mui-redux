4fb7f8414f9e44a63c58824808259dd9d67f9b90
mohammadreza <shahbaziweb@gmail.com>
2024-05-19 11:24:52 +0330

    create loading component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from "next/app";
 import { StoreProvider } from "@redux/StoreProvider";
 import { ThemeProvider } from "@theme/theme";
 import '@locales/i18n';
+import { Loading } from "@components/Loading";
 
 const App = ({Component, pageProps}: AppProps) => {
   return (
       <StoreProvider>
         <ThemeProvider>
           <Component {...pageProps} />
+          <Loading/>
         </ThemeProvider>
       </StoreProvider>
   )
