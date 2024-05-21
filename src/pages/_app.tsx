e0779f0d47284f47193635e96502aff403cd1371
mohammadreza <shahbaziweb@gmail.com>
2024-05-21 16:26:39 +0330

    show global toast on requests

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import type { AppProps } from "next/app";
 import { StoreProvider } from "@redux/StoreProvider";
 import { ThemeProvider } from "@theme/theme";
-import '@locales/i18n';
 import { Loading } from "@components/Loading";
+import { Toast } from "@components/Toast";
+import '@locales/i18n';
 
 const App = ({Component, pageProps}: AppProps) => {
   return (
@@ -10,6 +11,7 @@ const App = ({Component, pageProps}: AppProps) => {
         <ThemeProvider>
           <Component {...pageProps} />
           <Loading/>
+          <Toast/>
         </ThemeProvider>
       </StoreProvider>
   )
