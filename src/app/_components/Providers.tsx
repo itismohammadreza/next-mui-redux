799c0fa0d9da4addade49b0d5f4cdd2ce1057b82
mohammadreza <shahbaziweb@gmail.com>
2024-06-01 18:53:32 +0330

    create separate providers

--- src/app/_components/Providers.tsx
+++ src/app/_components/Providers.tsx
@@ -1,12 +1,12 @@
 'use client';
-
 import { ThemeProvider } from "@theme/theme";
 import { Loading } from "./Loading";
 import { Toast } from "./Toast";
 import { StoreProvider } from "@redux/StoreProvider";
+import { WithChildren } from "@models/common";
 import '@locales/i18n';
 
-export function Providers({children}) {
+export const Providers = ({children}: WithChildren) => {
   return (
       <StoreProvider>
         <ThemeProvider>
